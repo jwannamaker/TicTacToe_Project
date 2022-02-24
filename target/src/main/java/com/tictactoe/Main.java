577f564561339a847691373d5097b45c19c74401
Dan Mateos <dan.mateos10@yahoo.com>
2022-02-23 16:00:19 -0800

    Example base line for project

---
+++ target/src/main/java/com/tictactoe/Main.java
@@ -0,0 +1,23 @@
+package com.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader titleScreenFxmlLoader= new FXMLLoader(Main.class.getResource("TitleScreen.fxml"));
+        Scene scene = new Scene(titleScreenFxmlLoader.load(), 600, 500);
+        stage.setTitle("Tic-Tac-Toe");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
