aca0625c97eebdc63408e2a03fa9b1937e9222e7
Daniel Camacho <dcamacho4@saddleback.edu>
2022-03-01 08:47:03 -0800

    Minimax uses String array for algorithm.

--- src/main/java/com/tictactoe/MiniMax.java
+++ src/main/java/com/tictactoe/MiniMax.java
@@ -1,77 +1,76 @@
 package com.tictactoe;
-
 import javafx.scene.control.Label;
-
 import java.util.List;
 import java.util.Objects;
 
 public class MiniMax {
-    Boolean movesLeft(List<Label> box) {
+    private final String[] boardState = new String[9];
+
+    Boolean isMovesLeft(String[] boardState) {
         for (int i = 0; i < 9; ++i)
-            if (Objects.equals(box.get(i).getText(), ""))
+            if (Objects.equals(boardState[i], ""))
                 return true;
         return false;
     }
 
-    public int evaluate(List<Label> box) {
+    public int evaluate(String[] boardState) {
         for (int i = 0; i < 3; ++i) {
-            if (Objects.equals(box.get(3 * i).getText(), box.get(3 * i + 1).getText()) &&
-                    Objects.equals(box.get(3 * i).getText(), box.get(3 * i + 2).getText())) {
-                if (Objects.equals(box.get(3 * i).getText(), "X"))
+            if (Objects.equals(boardState[3 * i], boardState[3 * i + 1]) &&
+                    Objects.equals(boardState[3 * i], boardState[3 * i + 2])) {
+                if (Objects.equals(boardState[3 * i], "X"))
                     return 10;
-                else if (Objects.equals(box.get(3 * i).getText(), "O"))
+                else if (Objects.equals(boardState[3 * i], "O"))
                     return -10;
             }
-            if (Objects.equals(box.get(i).getText(), box.get(3 + i).getText()) &&
-                    Objects.equals(box.get(i).getText(), box.get(6 + i).getText())) {
-                if (Objects.equals(box.get(i).getText(), "X"))
+            if (Objects.equals(boardState[i], boardState[3 + i]) &&
+                    Objects.equals(boardState[i], boardState[6 + i])) {
+                if (Objects.equals(boardState[i], "X"))
                     return 10;
-                else if (Objects.equals(box.get(i).getText(), "O"))
+                else if (Objects.equals(boardState[i], "O"))
                     return -10;
             }
         }
-        if (Objects.equals(box.get(0).getText(), box.get(4).getText()) &&
-                Objects.equals(box.get(0).getText(), box.get(8).getText())) {
-            if (Objects.equals(box.get(0).getText(), "X"))
+        if (Objects.equals(boardState[0], boardState[4]) &&
+                Objects.equals(boardState[0], boardState[8])) {
+            if (Objects.equals(boardState[0], "X"))
                 return 10;
-            else if (Objects.equals(box.get(0).getText(), "O"))
+            else if (Objects.equals(boardState[0], "O"))
                 return -10;
         }
-        if (Objects.equals(box.get(6).getText(), box.get(4).getText()) &&
-                Objects.equals(box.get(6).getText(), box.get(2).getText())) {
-            if (Objects.equals(box.get(6).getText(), "X"))
+        if (Objects.equals(boardState[6], boardState[4]) &&
+                Objects.equals(boardState[6], boardState[2])) {
+            if (Objects.equals(boardState[6], "X"))
                 return 10;
-            else if (Objects.equals(box.get(6).getText(), "O"))
+            else if (Objects.equals(boardState[6], "O"))
                 return -10;
         }
         return 0;
     }
 
-
-    int miniMax(List<Label> box, int depth, Boolean isMax) {
-        int score = evaluate(box);
+    int miniMax(String[] boardState, int depth, Boolean isMax) {
+        int score = evaluate(boardState);
 
         if (score == 10) return score;
         if (score == -10) return score;
-        if (!movesLeft(box)) return 0;
+        if (!isMovesLeft(boardState)) return 0;
 
         if (isMax) {
             int maxEval = -999;
             for (int i = 0; i < 9; ++i) {
-                if (Objects.equals(box.get(i).getText(), "")) {
-                    box.get(i).setText("X");
-                    maxEval = Math.max(maxEval, miniMax(box, depth + 1, false));
-                    box.get(i).setText("");
+                if (Objects.equals(boardState[i], "")) {
+                    boardState[i] = "X";
+                    maxEval = Math.max(maxEval, miniMax(boardState, depth + 1, false));
+                    boardState[i] = "";
                 }
             }
             return maxEval;
         } else {
             int minEval = 999;
             for (int i = 0; i < 9; ++i) {
-                if (Objects.equals(box.get(i).getText(), "")) {
-                    box.get(i).setText("O");
-                    minEval = Math.min(minEval, miniMax(box, depth + 1, true));
-                    box.get(i).setText("");
+                if (Objects.equals(boardState[i], "")) {
+                    boardState[i] = "O";
+                    minEval = Math.min(minEval, miniMax(boardState, depth + 1, true));
+                    boardState[i] = "";
                 }
             }
             return minEval;
@@ -79,14 +78,17 @@ public class MiniMax {
     }
 
     public void computerPlayerMove(List<Label> box) {
+        for (int i = 0; i < 9; ++i)
+            boardState[i] = box.get(i).getText();
+
         int maxEval = -999;
         int pos = -1;
 
         for (int i = 0; i < 9; ++i) {
-            if (Objects.equals(box.get(i).getText(), "")) {
-                box.get(i).setText("X");
-                int moveEval = Math.max(maxEval, miniMax(box, 0, false));
-                box.get(i).setText("");
+            if (Objects.equals(boardState[i], "")) {
+                boardState[i] = "X";
+                int moveEval = Math.max(maxEval, miniMax(boardState, 0, false));
+                boardState[i] = "";
 
                 if (moveEval > maxEval)
                     pos = i;
